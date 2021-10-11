1402324f179b9fe76272c09cf48321a73eefa4eb
light <lightning_z@outlook.com>
2021-10-11 10:02:08 +0800

    mail

---
+++ src/main/java/com/newcoder/community/util/CommunityConstant.java
@@ -0,0 +1,14 @@
+package com.newcoder.community.util;
+
+public interface CommunityConstant {
+
+    //常量
+    //激活成功
+    int ACTIVATION_SUCCESS=0;
+
+    //重复激活
+    int ACTIVATION_REPEAT=1;
+
+    //激活失败
+    int ACTIVATION_FAILURE=2;
+}
---
+++ src/main/java/com/newcoder/community/util/CommunityUtil.java
@@ -0,0 +1,27 @@
+package com.newcoder.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    //激活码 随机字符串
+    public static String generateUUID()
+    {
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+    //MD5加密 加salt
+    public static String md5(String key)
+    {
+        if(StringUtils.isBlank(key))
+        {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+}
